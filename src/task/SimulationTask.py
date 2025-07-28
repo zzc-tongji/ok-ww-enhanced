5b585e1fbb016716f05a2dcb21fffed6c6a55889
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2025-07-28 11:51:13 +0800

    fix teleport in simulation

--- src/task/SimulationTask.py
+++ src/task/SimulationTask.py
@@ -50,7 +50,7 @@ class SimulationTask(DomainTask):
         self.wait_click_travel()
         self.wait_in_team_and_world(time_out=self.teleport_timeout)
         self.sleep(max(5, self.teleport_timeout / 10))
-        self.walk_until_f(time_out=1)
+        self.walk_until_f(time_out=2)
         self.pick_f()
         if selection == 'Resonator EXP':
             index = 0
